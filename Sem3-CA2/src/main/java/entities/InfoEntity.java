a43cfc3ffaaff5f7ce5f320aca9e0a1a1aa3594d
JonasSchack <jonas.schack.nielsen@gmail.com>
2017-03-15 10:24:29 +0100

    Added JDBC DRIVER and added the database to the linux server Created a test class to generate the tables

--- Sem3-CA2/src/main/java/entities/InfoEntity.java
+++ Sem3-CA2/src/main/java/entities/InfoEntity.java
@@ -12,6 +12,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
@@ -20,6 +22,7 @@ import javax.persistence.OneToMany;
  * @author Nicolai
  */
 @Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class InfoEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
